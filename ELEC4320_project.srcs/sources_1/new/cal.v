5cf53104b2e4511f8ea64dd558cb4081807ca152
Marco Tam <marcotamcn@gmail.com>
2025-10-29 22:42:43 +0800

    created an empty project

---
+++ ELEC4320_project.srcs/sources_1/new/cal.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 29.10.2025 22:42:07
+// Design Name: 
+// Module Name: cal
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module cal(
+
+    );
+endmodule
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ELEC4320-Project
+
+## Version of the Vivado Design
+
+This project was developed using Vivado 2024.1
